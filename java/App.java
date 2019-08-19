e845e6888d3a7d412f37a45bfafd447db22a54f8
jmz <mingzhuang.ji@ly.com>
2019-08-19 15:56:14 +0800

    java打包jar测试

---
+++ java/App.java
@@ -0,0 +1,10 @@
+import ming.Test;
+
+class App {
+  char sex = 's';
+  String name = "jmz";
+
+  public static void main(String[] args) {
+    Test.say();
+  }
+}
---
+++ java/ming/Test.java
@@ -0,0 +1,7 @@
+package ming;
+
+public class Test{
+  public static void say(){
+    System.out.println('j');
+  }
+}
