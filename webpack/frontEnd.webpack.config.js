3aacfa92b149b3119f0b8cbac04ca7de2f9334e7
jmz <mingzhuang.ji@ly.com>
2020-01-08 17:30:23 +0800

    去掉语法检查

--- webpack/frontEnd.webpack.config.js
+++ webpack/frontEnd.webpack.config.js
@@ -2,9 +2,6 @@ import webpackBase from './webpackConfig/base.config.js';
 import webpackDev from './webpackConfig/dev.config.js';
 import webpackPro from './webpackConfig/pro.config.js';
 
-
-
-
 // 开发环境
 if (process.env.NODE_ENV === 'development') {
     webpackBase.plugins.concat(webpackDev.plugins);
