9debc42be05e13224128527fd3acd8b60960dadf
jmz <aiyoudqrjmz@163.com>
2020-04-04 22:59:53 +0800

    两个有序数组合并

--- data-structure-algorithms/other/two-num-concat.js
+++ data-structure-algorithms/other/two-num-concat.js
@@ -10,4 +10,31 @@ function twoNumConcat(num1, num2) {
   return num1.concat(num2).filter(item => item).sort((a, b) => a - b);
 }
 
-console.log(twoNumConcat([1, 2, 3, 0, 0, 0], [0, 2, 2, 4, 5]));
+function twoNumsMerge(nums1, n, nums2, m) {
+  let len1 = n - 1, len2 = m - 1, len = n + m - 1;
+
+  while (len2 > 0) {
+    if (len1 < 0) {
+      nums1[len--] = nums2[len2--];
+      continue;
+    }
+    nums1[len--] = nums1[len1] >= nums2[len2] ? nums1[len1--] : nums2[len2--];
+  }
+  return nums1;
+}
+
+var merge = function(nums1, m, nums2, n) {
+  let len1 = m - 1,
+      len2 = n - 1,
+      len = m + n - 1
+  while(len2 >= 0) {
+      if(len1 < 0) {
+          nums1[len--] = nums2[len2--]
+          continue
+      }
+      nums1[len--] = nums1[len1] >= nums2[len2] ? nums1[len1--]: nums2[len2--]
+  }
+  return nums1;
+};
+
+console.log(twoNumsMerge([1, 2, 3,7,8,9], 6, [1,2,5,6], 4));
