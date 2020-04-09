7eb4c5a49d4394e3bf5c1795fb3c5c8c87fa5661
jmz <mingzhuang.ji@ly.com>
2020-04-09 09:10:23 +0800

    新增：图数据结构

--- data-structure-algorithms/graph/graph2.js
+++ data-structure-algorithms/graph/graph2.js
@@ -1,13 +1,32 @@
 /**
  * 图数据结构
+ * 
+ * 表达图数据结构，
+ * 1. 要用顶点和边的方案，使用对象表示顶点的方式随着顶点的增大，会暂用大量内存；
+ * 
+ * 2. 保存顶点和边
+ * 
+ * 3. 顶点保存再数组中，可以通过数组索引引用他们
  */
+
+ /**
+  * 顶点
+  * @param {*} label 
+  */
+function Vertex(label){
+  this.label = label;
+  this.wasVisited = false;
+}
+
 function Graph(v) {
   this.vertices = v;
   this.edges = 0;
   this.adj = [];
+  this.marked = [];
   for (let i = 0; i < this.vertices; i++) {
     this.adj[i] = [];
     this.adj[i].push('');
+    this.marked[i] = false;
   }
 }
 
@@ -25,9 +44,20 @@ Graph.prototype = {
         }
       }
     }
+  },
+  /**
+   * 深度优先搜索
+   * @param {*} v 
+   */
+  dfs(v) {
+    this.marked[v] = true;
+    if (this.adj[v] !== undefined) {
+      console.log('vertex:' + v);
+      for (let key in this.adj[v]) {
+        if (!this.marked[key]) {
+          this.dfs(key);
+        }
+      }
+    }
   }
 };
-
-/**
- * 搜索图
- */
