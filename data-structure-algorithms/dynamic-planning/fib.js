7abfd31f1f72251517dc449d66bc3ce9de5a9e5d
jmz <mingzhuang.ji@ly.com>
2020-04-17 09:11:39 +0800

    新增：贪心算法

--- data-structure-algorithms/dynamic-planning/fib.js
+++ data-structure-algorithms/dynamic-planning/fib.js
@@ -1,12 +1,18 @@
 /**
+ * 所有可以使用递归的地方都可以使用动态规划 
  * 动态规划版本的斐波那契数列
- * @param {*} n 
+ * 
+ * 0 1 1 2 3 5 8 第一个数位1
+ * 
+ * @param {*} n 从0开始
  */
 function recurFib(n) {
   // 通常需要将结果保存起来
   const val = [];
   // 设置一个数值长度的数组，初始值为0
-  for (let i = 0; i < n; i++) {
+  // 每个元素存储的是当前数列的值
+  // 
+  for (let i = 0; i <= n; i++) {
     val[i] = 0;
   }
   // 当n为1，或者2时 ，和为2，直接返回（边界情况
@@ -17,7 +23,7 @@ function recurFib(n) {
     val[1] = 1;
     val[2] = 2;
     for (var i = 3; i <= n; i++) {
-      // 开始根据斐波那契数列规则进行计算
+      // 开始根据斐波那契数列规则进行计算(保存到对应的元素中去)
       val[i] = val[i - 1] + val[i - 2];
     }
     // 返回数组的最后一个
@@ -29,17 +35,17 @@ function recurFib(n) {
  * 
  * @param {*} n 数列长度
  */
-function recurFib(n) {
-    let sum = 1;
-    let last = 0;
-    let next = 1;
-    for (var i = 2; i <= n; i++) {
-      // 开始根据斐波那契数列规则进行计算
-      sum = last + next;
-      last = next;
-      next = sum;
-    }
-    return sum;
-}
+// function recurFib(n) {
+//     let sum = 1;
+//     let last = 0;
+//     let next = 1;
+//     for (var i = 2; i <= n; i++) {
+//       // 开始根据斐波那契数列规则进行计算
+//       sum = last + next;
+//       last = next;
+//       next = sum;
+//     }
+//     return sum;
+// }
 
 console.log(recurFib(10));
--- data-structure-algorithms/recursion/fib.js
+++ data-structure-algorithms/recursion/fib.js
@@ -3,9 +3,10 @@
  * 斐波那契数列（规律：每个数都是前两个数之和，从第三个数开始），求到n的斐波那契数列的和； 
  * 0 1 1 2 3 5 8
  * 
- * @param {*} n 
+ * @param {*} n 第几位数值（从0开始
  */
 function recurFib(n) {
+  // 前两位数 0 1
   if (n < 2) {
     return n;
   }
@@ -13,4 +14,4 @@ function recurFib(n) {
   return recurFib(n - 1) + recurFib(n - 2)
 }
 
-console.log(recurFib(10));
+console.log(recurFib(0));
