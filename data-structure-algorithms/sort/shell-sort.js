9f7660b7db4cf096a8d33321154224d67933817b
jmz <mingzhuang.ji@ly.com>
2020-09-03 20:28:10 +0800

    english:

--- data-structure-algorithms/sort/shell-sort.js
+++ data-structure-algorithms/sort/shell-sort.js
@@ -1,17 +1,20 @@
 /**
  * 希尔排序
  * @param {array} arr 待排序数组
- * @param {array} gaps 排序间隔
+ * @param {array} gaps 排序间隔 ，当gps数组为[1]时，排序变为插入排序
  */
 function shellSort(arr, gaps) {
   for (let g = 0, len = gaps.length; g < len; g++) {
     for (let i = gaps[g], aLen = arr.length; i < aLen; i++) {
       let temp = arr[i];
-      let j = i;
-      for (; j >= gaps[g] && arr[j - gaps[g]] > temp; j -= gaps[g]) {
-        arr[j] = arr[j - gaps[g]];
+      let inner = i;
+
+      while (inner >= gaps[g] && arr[inner - gaps[g]] > temp) {
+        arr[inner] = arr[inner - gaps[g]];
+        inner = inner - gaps[g];
       }
-      arr[j] = temp;
+
+      arr[inner] = temp;
     }
   }
   return arr;
@@ -24,20 +27,28 @@ function shellSort(arr, gaps) {
 function shellSort1(arr) {
   const len = arr.length;
   let h = 1;
+
   while (len / 3 > h) {
     h = 3 * h + 1;
   }
+
   while (h >= 1) {
+
     for (let i = h; i < len; i++) {
       let j = i;
-      for (; j >= h && arr[j - h] > arr[j]; j -= h) {
+
+      for (; j >= h && arr[j - h] > arr[j]; ) {
         arr[j] = arr[j - h];
+        j = j- h;
       }
+
       arr[j] = arr[i];
     }
+
     h = (h - 1) / 3;
   }
+
   return arr;
 }
 
-console.log(shellSort([1, 2, 3, 8, 3, 4, 5, 1], [3, 1]));
+console.log(shellSort([1, 2, 3,8,4,6, 8, 3, 4, 5, 1], [1]));
