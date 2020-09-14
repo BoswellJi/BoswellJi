faa3a8d8050cd3b4de6c74450a9227ee175de953
jmz <mingzhuang.ji@ly.com>
2020-09-14 20:52:26 +0800

    webgl: 光照

--- html/canvas-3d/guide/light/index.js
+++ html/canvas-3d/guide/light/index.js
@@ -1,67 +1,10 @@
 /**
-  * 创建着色器程序
-  * @param {*} gl 渲染上下文
-  * @param {*} vertexShader 顶点着色器
-  * @param {*} fragmentShader 片段着色器
-  */
-function createProgram(gl, vertexShader, fragmentShader) {
-  const program = gl.createProgram();
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
-  gl.linkProgram(program);
-
-  const success = gl.getProgramParameter(program, gl.LINK_STATUS);
-  if (success) {
-    return program;
-  }
-
-  console.log('program: ' + gl.getProgramInfoLog(program));
-  gl.deleteProgram(program);
-}
-
-/**
- * 创建着色器
- * @param {*} gl 渲染上下文
- * @param {*} type 着色器类型
- * @param {*} source 数据源
- */
-function createShader(gl, type, source) {
-  const shader = gl.createShader(type);
-  gl.shaderSource(shader, source);
-  gl.compileShader(shader);
-
-  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-  if (success) {
-    return shader;
-  }
-
-  console.log('shader: ' + gl.getShaderInfoLog(shader));
-  gl.deleteShader(shader);
-}
-
-function initShaders(gl, vertex, fragment) {
-  //  创建两个着色器
-  const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertex),
-    fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragment);
-
-  // 将两个着色器link（链接）到一个 program 
-  const program = createProgram(gl, vertexShader, fragmentShader);
-
-  gl.useProgram(program);
-
-  gl.program = program;
-
-  return program;
-}
-
-/**
  * 初始化顶点数据缓存
  * 物体都是有，点，线面（三角形组成
  * @param {*} gl 
  */
 function initVertexBuffer(gl) {
-  // 24
-  var vertices = new Float32Array([   // Coordinates
+  var vertices = new Float32Array([
     1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, // v0-v1-v2-v3 front
     1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, // v0-v3-v4-v5 right
     1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, // v0-v5-v6-v1 up
@@ -70,18 +13,16 @@ function initVertexBuffer(gl) {
     1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0  // v4-v7-v6-v5 back
   ]);
 
-  // 
-  var colors = new Float32Array([    // Colors
-    1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,     // v0-v1-v2-v3 front
-    1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,     // v0-v3-v4-v5 right
-    1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,     // v0-v5-v6-v1 up
-    1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,     // v1-v6-v7-v2 left
-    1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,     // v7-v4-v3-v2 down
-    1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0　    // v4-v7-v6-v5 back
+  var colors = new Float32Array([
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  // v0-v1-v2-v3 front    
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // v0-v3-v4-v5 right
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // v0-v5-v6-v1 up
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // v1-v6-v7-v2 left
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // v7-v4-v3-v2 down
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 // v4-v7-v6-v5 back
   ]);
 
-
-  var normals = new Float32Array([    // Normal
+  var normals = new Float32Array([
     0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0,  // v0-v1-v2-v3 front
     1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0,  // v0-v3-v4-v5 right
     0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0,  // v0-v5-v6-v1 up
@@ -91,7 +32,7 @@ function initVertexBuffer(gl) {
   ]);
 
 
-  // Indices of the vertices，每个面的两个三角形的顶点索引
+  // 每个面的两个三角形的顶点索引
   var indices = new Uint8Array([
     0, 1, 2, 0, 2, 3,    // front
     4, 5, 6, 4, 6, 7,    // right
@@ -105,20 +46,17 @@ function initVertexBuffer(gl) {
   initArrayBuffer(gl, 'a_Color', colors, 3, gl.FLOAT);
   initArrayBuffer(gl, 'a_Normal', normals, 3, gl.FLOAT);
 
-  // 创建缓冲数据
+
   const indexBuffer = gl.createBuffer();
 
-  // 绑定缓冲
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-
-  // 将数据与缓冲进行绑定
   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
 
   return indices.length;
 }
 
 /**
- * 初始化数组类型缓冲
+ * 初始化数组类型缓冲区
  * @param {*} gl 
  * @param {*} attribute 
  * @param {*} data 
@@ -126,7 +64,6 @@ function initVertexBuffer(gl) {
  * @param {*} type 
  */
 function initArrayBuffer(gl, attribute, data, num, type) {
-  // 创建缓冲区，将获取的glsl变量的地址指向缓冲区
   const buffer = gl.createBuffer();
 
   gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
@@ -143,25 +80,30 @@ const canvas = document.querySelector('#canvas'),
   gl = canvas.getContext('webgl');
 
 function draw() {
+
+  // 平行光下的漫反射光
   const vertex = `
   attribute vec4 a_Position;
   attribute vec4 a_Color;
   attribute vec4 a_Normal; // 法向量
+
   uniform mat4 u_MvpMatrix;
   uniform vec3 u_LightColor; // 入射线/光线颜色
   uniform vec3 u_LightDirection; // 归一化世界坐标
+
   varying vec4 v_Color;
 
   void main(){
+    // 模型视图透视举证转换
     gl_Position = u_MvpMatrix * a_Position;
     // 对法向量进行归一化
     vec3 normal = normalize(vec3(a_Normal));
-    // 计算光线方向和法向量的点积
+    // 计算光线方向和法向量的点积(夹角)，当夹角大于90度时，光照没有作用，照射在物体背面
     float nDotL = max(dot(u_LightDirection,normal),0.0);
     // 计算漫反射光线的颜色
     vec3 diffuse = u_LightColor * vec3(a_Color) * nDotL;
     // 顶点颜色
-    v_Color = vec4(diffuse,a_Color);
+    v_Color = vec4(diffuse,1.0);
   }
   `,
     fragment = `
@@ -172,7 +114,7 @@ function draw() {
       gl_FragColor = v_Color;
     }`;
 
-  if (!initShaders(gl, vertex, fragment)) {
+  if (!initShaderProgram(gl, vertex, fragment)) {
     return;
   }
 
@@ -181,72 +123,29 @@ function draw() {
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.enable(gl.DEPTH_TEST);
 
+  // 可视空间下的物体
   const uMvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
+  // 视图投影矩阵
+  const mvpMatrix = new Matrix4();
+  // 透视矩阵
+  mvpMatrix.perspective(30, canvas.width / canvas.clientHeight, 1, 100);
+  // 视图矩阵
+  mvpMatrix.lookAt(3, 3, 7, 0, 0, 0, 0, 1, 0);
+  // 模型矩阵
+  mvpMatrix.scale(0.5,0.5,0.5);
+  gl.uniformMatrix4fv(uMvpMatrix, false, mvpMatrix.elements);
+
+  // 光照下的物体
+  // 光线颜色
   const uLightColor = gl.getUniformLocation(gl.program, 'u_LightColor');
-  const uLightDirection = gl.getUniformLocation(gl.program, 'u_LightDirection');
-
-  // 直接给存储位置添加数据，不使用缓冲区
-
-  // 设置光线颜色
   gl.uniform3f(uLightColor, 1, 1, 1);
+
   // 光线方向
+  const uLightDirection = gl.getUniformLocation(gl.program, 'u_LightDirection');
   const lightDirection = new Vector3([0.5, 3, 4]);
   lightDirection.normalize(); // 归一化
   gl.uniform3fv(uLightDirection, lightDirection.elements);
 
-  // 视图投影矩阵
-  const mvpMatrix = new Matrix4();
-  // 透视模型矩阵
-  mvpMatrix.setPerspective(30, canvas.width / canvas.clientHeight, 1, 100);
-  // 视图矩阵
-  mvpMatrix.lookAt(3, 3, 7, 0, 0, 0, 0, 1, 0);
-  gl.uniformMatrix4fv(uMvpMatrix, false, mvpMatrix.elements);
-
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-  gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
-}
-
-function initHandler(canvas, currentAngle) {
-  let dragging = false,
-    lastX = -1,
-    lastY = -1;
-
-  canvas.addEventListener('mousedown', function (e) {
-    let x = e.clientX,
-      y = e.clientY,
-      rect = e.target.getBoundingClientRect();
-    // 确保鼠标再canvas上
-    if (rect.left <= x && x < rect.right && rect.top <= y && y < rect.bottom) {
-      lastX = x;
-      lastY = y;
-      dragging = true;
-    }
-  });
-
-  canvas.addEventListener('mousemove', function (e) {
-    let x = e.clientX,
-      y = e.clientY;
-    // 确保鼠标再canvas上
-    if (dragging) {
-      let factor = 100 / canvas.height,
-        dx = factor * (x - lastX);
-      dy = factor * (y - lastY);
-
-      currentAngle[0] = Math.max(Math.min(currentAngle[0] + dy, 90), -90);
-      currentAngle[1] = currentAngle[1] + dx;
-    }
-    lastX = x;
-    lastY = y;
-  });
-}
-
-const g_MvpMatrix = new Matrix4();
-function matrixModify(gl, n, viewProjMatrix, uMvpMatrix, currentAngle) {
-  // g_MvpMatrix.set(viewProjMatrix);
-  g_MvpMatrix.rotate(currentAngle[0], 1, 0, 0);
-  g_MvpMatrix.rotate(currentAngle[1], 1, 0, 0);
-  gl.uniformMatrix4fv(uMvpMatrix, false, g_MvpMatrix.elements);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
 }
--- html/canvas-3d/guide/light/index4.js
+++ html/canvas-3d/guide/light/index4.js
@@ -4,6 +4,7 @@
  * @param {*} gl 
  */
 function initVertexBuffer(gl) {
+  // 顶点数量
   var vertices = new Float32Array([   // Coordinates
     1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, // v0-v1-v2-v3 front
     1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, // v0-v3-v4-v5 right
@@ -12,7 +13,8 @@ function initVertexBuffer(gl) {
     -1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0, // v7-v4-v3-v2 down
     1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0  // v4-v7-v6-v5 back
   ]);
- 
+
+  // 每个面的顶点的颜色
   var colors = new Float32Array([    // Colors
     1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,     // v0-v1-v2-v3 front
     1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,     // v0-v3-v4-v5 right
@@ -23,6 +25,7 @@ function initVertexBuffer(gl) {
   ]);
 
 
+  // 法线
   var normals = new Float32Array([    // Normal
     0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0,  // v0-v1-v2-v3 front
     1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0,  // v0-v3-v4-v5 right
@@ -49,74 +52,81 @@ function initVertexBuffer(gl) {
 
   // 创建缓冲数据
   const indexBuffer = gl.createBuffer();
+  // 绑定缓冲
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+  // 将数据与缓冲进行绑定
   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
 
   return indices.length;
 }
 
-/**
- * 初始化数组类型缓冲
- * @param {*} gl 
- * @param {*} attribute 
- * @param {*} data 
- * @param {*} num 
- * @param {*} type 
- */
-function initArrayBuffer(gl, attribute, data, num, type) {
-  // 创建缓冲区，将获取的glsl变量的地址指向缓冲区
-  const buffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
-
-  const aAttribute = gl.getAttribLocation(gl.program, attribute);
-  gl.vertexAttribPointer(aAttribute, num, type, false, 0, 0);
-  gl.enableVertexAttribArray(aAttribute);
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
-}
 
 const canvas = document.querySelector('#canvas'),
   gl = canvas.getContext('webgl');
 
 function draw() {
   const vertex = `
+  // 每个顶点都有的
   attribute vec4 a_Position;
   attribute vec4 a_Color;
-  attribute vec4 a_Normal; // 法向量
+  attribute vec4 a_Normal;      // 法向量
 
-  uniform mat4 u_MvpMatrix;
-  uniform vec3 u_LightColor; // 入射线颜色
-  uniform vec3 u_LightDirection; // 归一化世界坐标
-  uniform vec3 u_AmbientLight;
+  // 每个顶点都相同的
+  uniform mat4 u_ModelMatrix;   // 模型矩阵
+  uniform mat4 u_MvpMatrix;     // 透视投影矩阵 * 观察者矩阵
+  uniform mat4 u_NormalMatrix;  // 用来变换法向量的矩阵
   
+ 
   varying vec4 v_Color;
+  varying vec3 v_Normal;
+  varying vec3 v_Position;
 
   void main(){
+    // 顶点坐标
     gl_Position = u_MvpMatrix * a_Position;
 
-    // 对法向量进行归一化
-    vec3 normal = normalize(vec3(a_Normal));
+    // 变换后顶点的坐标
+    v_Position = vec3(u_ModelMatrix * a_Position);
 
-    // 计算光线方向和法向量的点积（入射角
-    float nDotL = max(dot(u_LightDirection,normal),0.0);
-
-    // 计算漫反射光线的颜色
-    vec3 diffuse = u_LightColor * vec3(a_Color) * nDotL;
-
-    // 环境反射光颜色 * 物体基底色
-    vec3 ambient = u_AmbientLight * a_Color.rgb;
+    // 顶点法线： 逆转置矩阵 * 源
+    v_Normal = normalize(vec3(u_NormalMatrix * a_Normal));
 
     // 顶点颜色
-    v_Color = vec4(diffuse + ambient,a_Color);
+    v_Color = a_Color;
   }
-  `,
-    fragment = `
+  `;
+
+  // 逐片元计算光照
+  const fragment = `
     precision mediump float;
+
+    uniform vec3 u_LightPosition; // 光源位置
+    uniform vec3 u_AmbientLight;  // 环境光
+    uniform vec3 u_LightDirection;  // 光照方向
+    uniform vec3 u_LightColor;  // 光照颜色
+    
     varying vec4 v_Color;
+    // 逐片元的的法线/法向量 片元法向量
+    varying vec3 v_Normal;
+    varying vec3 v_Position;
 
     void main() {
-      gl_FragColor = v_Color;
-    }`;
+  
+      // 点光源在顶点处的方向
+      vec3 lightDirection = normalize(u_LightPosition - vec3(v_Position));
+  
+      // 计算光线方向和法向量的点积（入射角
+      float nDotL = max(dot(lightDirection,v_Normal),0.0); 
+  
+      // 计算漫反射光线的颜色
+      vec3 diffuse = u_LightColor * vec3(v_Color) * nDotL;
+  
+      // 环境反射光颜色 * 物体基底色
+      vec3 ambient = u_AmbientLight * v_Color.rgb;
+  
+      gl_FragColor = vec4(diffuse + ambient,v_Color);
+    }
+    `;
 
   if (!initShaderProgram(gl, vertex, fragment)) {
     return;
@@ -127,7 +137,11 @@ function draw() {
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.enable(gl.DEPTH_TEST);
 
-  // 平行光线颜色
+  // 光源位置
+  const uLightPosition = gl.getUniformLocation(gl.program, 'u_LightPosition');
+  gl.uniform3f(uLightPosition, 2.3, 4.0, 3.5);
+
+  // 设置光线颜色
   const uLightColor = gl.getUniformLocation(gl.program, 'u_LightColor');
   gl.uniform3f(uLightColor, 1, 1, 1);
 
@@ -135,24 +149,51 @@ function draw() {
   const uAmbientLight = gl.getUniformLocation(gl.program, 'u_AmbientLight');
   gl.uniform3f(uAmbientLight, 0.2, 0.2, 0.2);
 
-  // 平行光方向
-  const uLightDirection = gl.getUniformLocation(gl.program, 'u_LightDirection');
-  const lightDirection = new Vector3([0.5, 3, 4]);
-  lightDirection.normalize(); // 归一化
-  gl.uniform3fv(uLightDirection, lightDirection.elements);
-
-  
-  // 模型视图投影矩阵
-  const uMvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
-  const mvpMatrix = new Matrix4();
   // 透视模型矩阵
-  mvpMatrix.setPerspective(30, canvas.width / canvas.clientHeight, 1, 100);
-  // 观察者状态矩阵
-  mvpMatrix.lookAt(3, 3, 7, 0, 0, 0, 0, 1, 0);
-  gl.uniformMatrix4fv(uMvpMatrix, false, mvpMatrix.elements);
+  const uMvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
+  const vpMatrix = new Matrix4();
+  vpMatrix.setPerspective(30, canvas.width / canvas.clientHeight, 1, 100);
+  vpMatrix.lookAt(6, 6, 14, 0, 0, 0, 0, 1, 0);
 
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-  gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
+  // 计算模型视图投影矩阵
+  const uModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
+  const modelMatrix = new Matrix4();
+  const mvpMatrix = new Matrix4();
+  // 计算变换后的法向量
+  const uNormalMatrix = gl.getUniformLocation(gl.program, 'u_NormalMatrix');
+  const normalMatrix = new Matrix4();
+
+
+  let currentAngle = 0.0;
+  const tick = function () {
+    currentAngle = animate(currentAngle);
+
+    modelMatrix.setRotate(currentAngle, 0, 1, 0);
+    gl.uniformMatrix4fv(uModelMatrix, false, modelMatrix.elements);
+
+    mvpMatrix.set(vpMatrix).multiply(modelMatrix);
+    gl.uniformMatrix4fv(uMvpMatrix, false, mvpMatrix.elements);
+
+    normalMatrix.setInverseOf(modelMatrix);
+    normalMatrix.transpose();
+    gl.uniformMatrix4fv(uNormalMatrix, false, normalMatrix.elements);
+
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
+
+    requestAnimationFrame(tick, canvas);
+  };
+  tick();
+}
+const ANGLE_STEP = 30.0;
+let g_last = Date.now();
+
+function animate(angle) {
+  let now = Date.now();
+  let elapsed = now - g_last;
+  g_last = now;
+  let newAngle = angle + (ANGLE_STEP * elapsed) / 1000.0;
+  return newAngle %= 360;
 }
 
 draw();
