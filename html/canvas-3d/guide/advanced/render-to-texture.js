0ec70fb79953657386014d9683d1cb8003b6570e
jmz <mingzhuang.ji@ly.com>
2020-09-28 20:13:49 +0800

    webgl:帧缓冲区

--- html/canvas-3d/guide/advanced/render-to-texture.js
+++ html/canvas-3d/guide/advanced/render-to-texture.js
@@ -1,3 +1,287 @@
 /**
  * 渲染3D图像，将渲染结果作为纹理贴到另一个三维物体上；
  */
+
+const vs = `
+  attribute vec4 a_Position;
+  attribute vec2 a_TexCoord;
+
+  uniform mat4 u_MvpMatrix;
+
+  varying vec2 v_TexCoord;
+
+  void main(){
+    gl_Position = u_MvpMatrix * a_Position;
+    v_TexCoord = a_TexCoord;
+  }
+ `;
+
+const fs = `
+  precision mediump float;
+  uniform sampler2D u_Sampler;
+  varying vec2 v_TexCoord;
+
+  void main(){
+    gl_FragColor = texture2D(u_Sampler,v_TexCoord);
+  }
+ `;
+
+const canvas = document.querySelector('#canvas');
+const gl = canvas.getContext('webgl');
+
+initShaders(gl, vs, fs);
+
+const program = gl.program;
+
+program.aPosition = gl.getAttribLocation(program, 'a_Position');
+program.aTexCoord = gl.getAttribLocation(program, 'a_TexCoord');
+program.uMvpMatrix = gl.getUniformLocation(program, 'u_MvpMatrix');
+
+const cube = initVertexBuffersForCube(gl);
+const plane = initVertexBuffersForPlane(gl);
+
+const texture = initTextures(gl);
+
+const fbo = initFramebufferObject(gl);
+
+gl.enable(gl.DEPTH_TEST);
+
+var viewProjMatrix = new Matrix4();   // Prepare view projection matrix for color buffer
+viewProjMatrix.setPerspective(30, canvas.width / canvas.height, 1.0, 100.0);
+viewProjMatrix.lookAt(0.0, 0.0, 7.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
+var viewProjMatrixFBO = new Matrix4();   // Prepare view projection matrix for FBO
+viewProjMatrixFBO.setPerspective(30.0, 400 / 400, 1.0, 100.0);
+viewProjMatrixFBO.lookAt(0.0, 2.0, 7.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
+// Coordinate transformation matrix
+var g_modelMatrix = new Matrix4();
+var g_mvpMatrix = new Matrix4();
+
+// Start drawing
+var currentAngle = 0.0; // Current rotation angle (degrees)
+var tick = function () {
+  currentAngle = animate(currentAngle);  // Update current rotation angle
+  draw(gl, canvas, fbo, plane, cube, currentAngle, texture, viewProjMatrix, viewProjMatrixFBO);
+  window.requestAnimationFrame(tick, canvas);
+};
+tick();
+
+var ANGLE_STEP = 30;   // The increments of rotation angle (degrees)
+
+var last = Date.now(); // Last time that this function was called
+function animate(angle) {
+  var now = Date.now();   // Calculate the elapsed time
+  var elapsed = now - last;
+  last = now;
+  // Update the current rotation angle (adjusted by the elapsed time)
+  var newAngle = angle + (ANGLE_STEP * elapsed) / 1000.0;
+  return newAngle % 360;
+}
+
+function draw(gl, canvas, fbo, plane, cube, angle, texture, viewProjMatrix, viewProjMatrixFBO) {
+  
+  gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+  gl.viewport(0, 0, 400, 400);
+
+  gl.clearColor(.2,.2,.4,1);
+  gl.clear(gl.COLOR_BUUFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  drawTexturedCube(gl, gl.program, cube, angle, texture, viewProjMatrixFBO);   // Draw the cube
+
+  gl.bindFramebuffer(gl.FRAMEBUFFER, null);        // Change the drawing destination to color buffer
+  gl.viewport(0, 0, canvas.width, canvas.height);  // Set the size of viewport back to that of <canvas>
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); // Clear the color buffer
+
+  // drawTexturedPlane(gl, gl.program, plane, angle, fbo.texture, viewProjMatrix);  // Draw the plane
+}
+
+function drawTexturedCube(gl, program, o, angle, texture, viewProjMatrix) {
+  // Calculate a model matrix
+  g_modelMatrix.setRotate(20.0, 1.0, 0.0, 0.0);
+  g_modelMatrix.rotate(angle, 0.0, 1.0, 0.0);
+
+  // Calculate the model view project matrix and pass it to u_MvpMatrix
+  g_mvpMatrix.set(viewProjMatrix);
+  g_mvpMatrix.multiply(g_modelMatrix);
+  gl.uniformMatrix4fv(program.u_MvpMatrix, false, g_mvpMatrix.elements);
+
+  drawTexturedObject(gl, program, o, texture);
+}
+
+function drawTexturedPlane(gl, program, o, angle, texture, viewProjMatrix) {
+  // Calculate a model matrix
+  g_modelMatrix.setTranslate(0, 0, 1);
+  g_modelMatrix.rotate(0, 1.0, 0.0, 0.0);
+  g_modelMatrix.rotate(angle, 0.0, 1.0, 0.0);
+
+  // Calculate the model view project matrix and pass it to u_MvpMatrix
+  g_mvpMatrix.set(viewProjMatrix);
+  g_mvpMatrix.multiply(g_modelMatrix);
+  gl.uniformMatrix4fv(program.u_MvpMatrix, false, g_mvpMatrix.elements);
+
+  drawTexturedObject(gl, program, o, texture);
+}
+
+function drawTexturedObject(gl, program, o, texture) {
+  // Assign the buffer objects and enable the assignment
+  initAttributeVariable(gl, program.aPosition, o.vertexBuffer);    // Vertex coordinates
+  initAttributeVariable(gl, program.aTexCoord, o.texCoordsBuffer);  // Texture coordinates
+
+  // Bind the texture object to the target
+  gl.activeTexture(gl.TEXTURE0);
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  // Draw
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, o.indexBuffer);
+  gl.drawElements(gl.TRIANGLES, o.numIndices, o.indexBuffer.type, 0);
+}
+
+// Assign the buffer objects and enable the assignment
+function initAttributeVariable(gl, a_attribute, buffer) {
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.vertexAttribPointer(a_attribute, buffer.num, buffer.type, false, 0, 0);
+  gl.enableVertexAttribArray(a_attribute);
+}
+
+function initVertexBuffersForPlane(gl) {
+  var vertices = new Float32Array([
+    1.0, 1.0, 0.0, -1.0, 1.0, 0.0, -1.0, -1.0, 0.0, 1.0, -1.0, 0.0    // v0-v1-v2-v3
+  ]);
+
+  // Texture coordinates
+  var texCoords = new Float32Array([1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0]);
+
+  // Indices of the vertices
+  var indices = new Uint8Array([0, 1, 2, 0, 2, 3]);
+
+  var o = new Object(); // Create the "Object" object to return multiple objects.
+
+  // Write vertex information to buffer object
+  // 顶点缓冲区
+  o.vertexBuffer = initArrayBufferForLaterUse(gl, vertices, 3, gl.FLOAT);
+  // 纹理坐标缓冲区
+  o.texCoordBuffer = initArrayBufferForLaterUse(gl, texCoords, 2, gl.FLOAT);
+  // 顶点索引缓冲区
+  o.indexBuffer = initElementArrayBufferForLaterUse(gl, indices, gl.UNSIGNED_BYTE);
+  if (!o.vertexBuffer || !o.texCoordBuffer || !o.indexBuffer) return null;
+
+  o.numIndices = indices.length;
+
+  // Unbind the buffer object
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+  return o;
+}
+
+function initVertexBuffersForCube(gl) {
+  // Vertex coordinates
+  var vertices = new Float32Array([
+    1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0,    // v0-v1-v2-v3 front
+    1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0,    // v0-v3-v4-v5 right
+    1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0,    // v0-v5-v6-v1 up
+    -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 1.0,    // v1-v6-v7-v2 left
+    -1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0,    // v7-v4-v3-v2 down
+    1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0     // v4-v7-v6-v5 back
+  ]);
+
+  // Texture coordinates
+  var texCoords = new Float32Array([
+    1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0,    // v0-v1-v2-v3 front
+    0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0,    // v0-v3-v4-v5 right
+    1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0,    // v0-v5-v6-v1 up
+    1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0,    // v1-v6-v7-v2 left
+    0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,    // v7-v4-v3-v2 down
+    0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0     // v4-v7-v6-v5 back
+  ]);
+
+  // Indices of the vertices
+  var indices = new Uint8Array([
+    0, 1, 2, 0, 2, 3,    // front
+    4, 5, 6, 4, 6, 7,    // right
+    8, 9, 10, 8, 10, 11,    // up
+    12, 13, 14, 12, 14, 15,    // left
+    16, 17, 18, 16, 18, 19,    // down
+    20, 21, 22, 20, 22, 23     // back
+  ]);
+
+  const o = new Object();
+
+  o.vertexBuffer = initArrayBufferForLaterUse(gl, vertices, 3, gl.FLOAT);
+  o.texCoordsBuffer = initArrayBufferForLaterUse(gl, texCoords, 2, gl.FLOAT);
+  o.indexBuffer = initElementArrayBufferForLaterUse(gl, indices, gl.UNSIGNED_BYTE);
+
+  o.numIndices = indices.length;
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+  return o;
+}
+
+function initArrayBufferForLaterUse(gl, data, num, type) {
+  const buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+  buffer.num = num;
+  buffer.type = type;
+  return buffer;
+}
+
+function initElementArrayBufferForLaterUse(gl, data, type) {
+  const buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, data, gl.STATIC_DRAW);
+  gl.type = type;
+  return buffer;
+}
+
+function initTextures(gl) {
+  const texture = gl.createTexture();
+  const uSampler = gl.getUniformLocation(program, 'u_Sampler');
+  const image = new Image();
+
+  image.onload = function () {
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.uniform1i(uSampler, 0);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+  }
+
+  image.src = 'http://localhost/canvas-3d/guide/asset/sky.jpg';
+  return texture;
+}
+
+function initFramebufferObject(gl) {
+  const framebuffer = gl.createFramebuffer();
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 400, 400, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+  framebuffer.texture = texture;
+  const depthBuffer = gl.createRenderbuffer();
+
+  gl.bindRenderbuffer(gl.RENDERBUFFER, depthBuffer);
+  gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, 400, 400);
+
+  gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
+
+  gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
+  gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, depthBuffer);
+
+  const e = gl.checkFramebufferStatus(gl.FRAMEBUFFER);
+
+  console.log(e);
+
+  gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+  gl.bindTexture(gl.TEXTURE_2D, null);
+  gl.bindRenderbuffer(gl.RENDERBUFFER, null);
+
+  return framebuffer;
+}
