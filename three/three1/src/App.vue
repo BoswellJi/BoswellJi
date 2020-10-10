9abb1e832023cfcb8a7629f5c9f23c34fccf4191
jmz <mingzhuang.ji@ly.com>
2020-10-10 20:58:31 +0800

    webgl:vue+three

---
+++ three/three1/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">three</div>
+</template>
+
+<script>
+import { Scene, Camera } from "three";
+
+export default {
+  name: "App",
+  mounted() {
+    const scene = new Scene();
+  },
+};
+</script>
+
+<style>
+</style>
