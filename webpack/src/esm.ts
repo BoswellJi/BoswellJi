0f397e27a38dbf52be3b1d89823dddcb46666e78
Boswell <aiyoudqrjmz@163.com>
2022-04-03 19:27:17 +0800

    feat: esm中原始数据类型变量变更问题

--- webpack/src/esm.ts
+++ webpack/src/esm.ts
@@ -1,7 +1,13 @@
+
 export default function <T extends number>(a: T, b: T) {
   console.log(a + b,'esm');
+  num++;
 }
 
-export function esmAdd(a: string): string {
-  return a;
+// 原始类型的变量，在别的模块中通过方法调用修改后，可获取最新值
+export let num : number = 0;
+
+export function esmAdd(): number {
+   num++;
+   return num;
 }
--- webpack/src/module3.vue
+++ webpack/src/module3.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
     <router-view></router-view>
-    {{ name }}
+    <button @click="setState">{{ name }} {{num}}</button>
   </div>
 </template>
 
 <script lang="ts" setup>
   import { ref } from 'vue';
+  import add,{ esmAdd, num } from './esm';
 
   const name = ref('hhh');
+
+  function setState() {
+    // esmAdd();
+    add();
+    console.log(num);
+  }
 </script>
