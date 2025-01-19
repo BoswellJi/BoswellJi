e572069c0ed7cc596652df1632b219f2c22998fc
Boswell <aiyoudqrjmz@163.com>
2025-01-19 19:30:53 +0800

    chore: 安装依赖

--- packages/blog/vuepress.config.ts
+++ packages/blog/vuepress.config.ts
@@ -1,5 +1,5 @@
 import { viteBundler } from '@vuepress/bundler-vite'
-import defaultTheme from '@vuepress/theme-default'
+import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
